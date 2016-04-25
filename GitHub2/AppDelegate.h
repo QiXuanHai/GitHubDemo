db3ad0c3c23c698620ced9c8c60c7c344bb76e6c
heiheihei <1526604578@qq.com>
2016-04-25 15:53:20 +0800

    third commit

---
+++ GitHub2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHub2
+//
+//  Created by qf1 on 16/4/25.
+//  Copyright (c) 2016年 梁国海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHub2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHub2
+//
+//  Created by qf1 on 16/4/25.
+//  Copyright (c) 2016年 梁国海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
